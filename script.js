8475091325f32685ec31cd0baa2693aeb8663c7c
Cwiz <kristinapronina795@gmail.com>
2025-09-22 17:12:40 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const sheetUrls = [
   // 1-й слайд: таблица
-  https://docs.google.com/spreadsheets/u/0/d/1c-4qaUYLn5MuFN1b1rfpmPva8ZpXzAWiM3mHS3BxOdo/export?format=csv&id=1c-4qaUYLn5MuFN1b1rfpmPva8ZpXzAWiM3mHS3BxOdo&gid=0,
+  
+ https://docs.google.com/spreadsheets/u/0/d/1c-4qaUYLn5MuFN1b1rfpmPva8ZpXzAWiM3mHS3BxOdo/export?format=csv&id=1c-4qaUYLn5MuFN1b1rfpmPva8ZpXzAWiM3mHS3BxOdo&gid=0,
   // 2-й слайд: лидер дня
- https://docs.google.com/spreadsheets/u/0/d/1dGhcG5VwKJOL-mrX4ovBT3gYO-XYc55kWpEgqMlNefM/export?format=csv&id=1dGhcG5VwKJOL-mrX4ovBT3gYO-XYc55kWpEgqMlNefM&gid=0
+  
+https://docs.google.com/spreadsheets/u/0/d/1dGhcG5VwKJOL-mrX4ovBT3gYO-XYc55kWpEgqMlNefM/export?format=csv&id=1dGhcG5VwKJOL-mrX4ovBT3gYO-XYc55kWpEgqMlNefM&gid=0
 ];
 
 let currentIndex = 0;
@@ -141,5 +143,6 @@ setInterval(() => {
 }, 10000);
 
 loadAllSheets();
+
 
 
